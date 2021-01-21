67827f5c71d5ea7475d7fcd012aff81634bd8a71
IVANOV <79323@DESKTOP-7NSDFUG>
2021-01-21 11:47:39 +0500

    Добавьте файлы проекта.

---
+++ lab_rab_N2/lab_rab_N2.cpp
@@ -0,0 +1,20 @@
+﻿
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	int sum = 0;
+	for (int i = 1; i < 200; i += 2)
+	{
+		if (i % 2 == 1)
+			sum += 1;
+	}
+	cout << "Rezult " << sum << endl;
+	system("pause");
+	return 0;
+}
+
+
